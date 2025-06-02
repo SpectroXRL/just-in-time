eafab65f095f87cd68d98c5413bbabd5818e97c4
SpectroXRL <spectrothedev@gmail.com>
2025-06-02 01:29:32 -0500

    Model Modifications creating EF Core link between Subscriptions and Category Models.

--- JustInTime.Api/JustInTime.Api/Models/Category.cs
+++ JustInTime.Api/JustInTime.Api/Models/Category.cs
@@ -4,5 +4,6 @@
     {
         public Guid Id { get; set; }
         public required string Name { get; set; }
+        public ICollection<Subscription> Subscriptions { get; set; } = new List<Subscription>();
     }
 }
