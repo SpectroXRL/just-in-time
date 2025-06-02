e8a0c1b7d9f635944fc5c82f33c313b9e1f85df8
SpectroXRL <spectrothedev@gmail.com>
2025-06-01 21:37:01 -0500

    Setup Models for SubscriptionEndpoints

---
+++ JustInTime.Api/JustInTime.Api/Models/Subscription.cs
@@ -0,0 +1,13 @@
+﻿namespace JustInTime.Api.Models
+{
+    public class Subscription
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public decimal Cost { get; set; }
+        public Cycle Cycle { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly NextPaymentDate { get; set; }
+        public required Category Category { get; set; }
+    }
+}
